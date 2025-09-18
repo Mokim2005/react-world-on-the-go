4e6783a431fdfcf5110ad280025e491cf385e856
mokim <mamokim2005@gmail.com>
2025-09-19 00:50:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Contries from './components/contries'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Contries></Contries>
+      <h1>React World on the go...</h1>
+      
+    </>
+  )
+}
+
+export default App
